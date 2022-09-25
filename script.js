03f0969720520610e38e2d175553e314faacc4e4
Mofejesu Famuyiwa <niciepearl@gmail.com>
2022-09-25 16:46:18 +0100

    day 3 assignment

---
+++ script.js
@@ -0,0 +1,21 @@
+let multiply = "40";
+multiply *= "20";
+document.getElementById('ans').innerHTML=multiply;
+
+let value = "60";
+value *= "40";
+value *= "80";
+document.getElementById('value').innerHTML=value;
+
+let x = "10";
+x /= "5";
+document.getElementById('result').innerHTML=x;
+
+let y = "12";
+y /= "4";
+y /= "2";
+document.getElementById('returnvalue').innerHTML=y;
+
+let a = "100"
+a %= "20"
+document.getElementById('outcome').innerHTML=a;
